d9535a8bd7ab74f83b9eff2d54a6d7269005af8c
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-11 17:15:53 -0700

    main: add dinos diretory structure

--- routes/dinos.js
+++ routes/dinos.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Dinos baby');
+  res.render('dinos/index', {   
+    dinos: []
+  });
 });
 
 module.exports = router;
