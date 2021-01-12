4ee490c5c35d5c1b60dd725a77faa764981217e1
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-11 17:29:29 -0700

    main: import dinos

--- routes/dinos.js
+++ routes/dinos.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
+
+
 router.get('/', (req, res) => {
+  let dinos = fs.readFileSync('./dinos.json');
+  console.log(`dinos: `, dinos);
+
   res.render('dinos/index', {   
     dinos: []
   });
