953f85eee9b1a47a2c35ab5f20d90bda9ef39e60
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-11 17:56:48 -0700

    main: keeping up

--- routes/dinos.js
+++ routes/dinos.js
@@ -21,7 +21,9 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body);
-  res.send('Posting a dino');
+  // Add dinos to dino.json
+  
+  res.redirect('/dinos'); // Redirect
 });
 
 
